66286ee178e7de3aee505a05b73ace84dfce9362
jhyeep <45350427+jhyeep@users.noreply.github.com>
2020-09-16 12:38:32 +0800

    removed url length limit

--- urlapp/models.py
+++ urlapp/models.py
@@ -5,8 +5,8 @@ from hashlib import md5
 # Create your models here.
 class UrlEntry(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    full_url = models.URLField(unique=True)
-    url_hash = models.TextField(unique=True)
+    full_url = models.TextField()
+    url_hash = models.TextField()
 
     def save(self, *args, **kwargs):
         if not self.id:
